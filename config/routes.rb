79590be2f559bb0b34b3c3e65f10ede2da78c609
yuichi kawasaki <jiv.yu1.kawasaki@gmail.com>
2021-03-16 21:15:45 +0900

    fix deployment issues

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root  'problems#index'
 
-    devise_for :users, controllers: {
-      sessions: "users/sessions",
-      registrations: "users/registrations",
-      omniauth_callbacks: "users/omniauth_callbacks",
-      passwords: 'users/passwords',
-      # confirmations: "confirmations"
-    }
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks",
+    passwords: 'users/passwords',
+    # confirmations: "confirmations"
+  }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
   resources :users, only: [:show]
   resources :labels
