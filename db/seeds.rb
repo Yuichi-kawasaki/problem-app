bb61e94245c7797f833983d3ac01e5a3f78861d7
yuichi kawasaki <jiv.yu1.kawasaki@gmail.com>
2021-03-20 14:32:49 +0900

    herokuデプロイ

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,18 +18,12 @@ Problem.seed(
   { id: 2, user_id: 3, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
   { id: 3, user_id: 3, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
   { id: 4, user_id: 4, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
-  { id: 5, user_id: 1, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
+  { id: 5, user_id: 10, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
   { id: 6, user_id: 5, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
   { id: 7, user_id: 6, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
   { id: 8, user_id: 7, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
-  { id: 9, user_id: 2, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
-  { id: 10, user_id: 8, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
-  { id: 11, user_id: 1, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
-  { id: 12, user_id: 7, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
-  { id: 13, user_id: 10, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
-  { id: 14, user_id: 2, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
-  { id: 15, user_id: 2, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
-  { id: 16, user_id: 1, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') }
+  { id: 9, user_id: 8, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') },
+  { id: 10, user_id: 9, title: Faker::Book.title, content: Faker::Markdown.random, image: Faker::Avatar.image('my-own-slug') }
 )
 
 # Admin.create(
@@ -41,10 +35,9 @@ Problem.seed(
   Label.create(title:'新規' )
   Label.create(title:'リピート' )
   Label.create(title:'IT関連' )
-  Label.create(title:'使わなくなった物がある' )
-  Label.create(title:'欲しいものがある' )
   Label.create(title:'近所トラブル' )
   Label.create(title:'職場トラブル' )
   Label.create(title:'買い物トラブル' )
   Label.create(title:'コロナ関連' )
   Label.create(title:'趣味関連' )
+  Label.create(title:'その他' )
