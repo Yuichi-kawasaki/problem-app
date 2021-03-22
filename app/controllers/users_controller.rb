de915cd0d018e3da05af381f8095b78535591fcd
yuichi kawasaki <jiv.yu1.kawasaki@gmail.com>
2021-03-22 21:14:26 +0900

    herokuデプロイ再2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
      @user = User.find(params[:id])
-     # @problem = Problem.find(params[:id])
+     @problem = Problem.find(params[:id])
   end
 
   private
