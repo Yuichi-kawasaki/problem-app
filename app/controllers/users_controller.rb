9849320edd341b5ba2ad0a41b39b221ef5cfb07a
yuichi kawasaki <jiv.yu1.kawasaki@gmail.com>
2021-03-25 15:56:57 +0900

    profileのpath修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
 
   def show
      @user = User.find(params[:id])
-     @problem = Problem.find(params[:id])
+     if @user.problems.present?
+       @problem = Problem.find(params[:id])
+     end
   end
 
   private
