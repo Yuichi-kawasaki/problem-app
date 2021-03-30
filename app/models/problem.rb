648ede1b19bc77f96a71522ea901c46713b38c7d
yuichi kawasaki <jiv.yu1.kawasaki@gmail.com>
2021-03-30 12:52:19 +0900

    レイアウト崩れ防止

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,7 +1,7 @@
 class Problem < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :title, presence: true, length: {maximum: 30}
-  validates :content, presence: true, length: {maximum: 100}
+  validates :content, presence: true, length: {maximum: 300}
   belongs_to :user
   has_many :likes
   has_many :liked_users, through: :likes, source: :user
