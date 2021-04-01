cea40d9423c3ac2b5bcf84b267d4603195275d3b
yuichi kawasaki <jiv.yu1.kawasaki@gmail.com>
2021-04-01 20:41:55 +0900

    c

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,6 @@
 server '13.113.47.26', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/yuichikawasaki/.ssh/id_rsa'
+set :ssh_options, {
+  keys: '/Users/yuichikawasaki/.ssh/id_rsa',
+  forward_agent: false,
+  auth_methods: %w(publickey)
+}
